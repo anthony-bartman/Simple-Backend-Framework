b07e42cd297f8fc2b7555afd75a123958c86650c
Anthony Bartman <bartmanaj@msoe.edu>
2021-01-14 10:05:39 -0600

    Projects 1-2

---
+++ Lab1/udpechoclient.c
@@ -0,0 +1,18 @@
+// Simple UDP echo client
+// CE4961, Anthony Bartman
+//
+// Build with gcc -o udpechoclient udpechoclient.c
+// https://vcansimplify.wordpress.com/2013/03/14/c-socket-tutorial-echo-server/ 
+
+#include <stdio.h>      
+#include <stdlib.h>     
+#include <unistd.h> 
+#include <string.h> 
+#include <sys/types.h>  
+#include <sys/socket.h> 
+#include <netinet/in.h> 
+#include <arpa/inet.h> 
+
+int main(int argc, char** argv) {
+    
+}
